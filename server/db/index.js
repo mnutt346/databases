f3fa0a43132d310e14a6e664aca098cdbd8b42b2
mnutt346 <mnutt346@gmail.com>
2018-12-05 16:57:53 -0600

    Create chat DB with users, rooms, and messages tables.

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,15 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var dbConnection= mysql.createConnection({
+    host: "localhost",
+    user: "student",
+    database: 'chat'
+  });
 
+dbConnection.connect = (err) => {
+    if(err) {
+        console.log('ERROR CONNECTING TO CHAT DB: ', err);
+    }
+    console.log("CONNECTED TO CHAT DB!!!!!!");
+}
--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,44 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20),
+  message_text VARCHAR(400),
+  roomname VARCHAR(40),
+  user_key int,
+  room_key int
+  -- FOREIGN KEY (user_key) REFERENCES users (id),
+  -- FOREIGN KEY (room_key) REFERENCES rooms (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+DROP TABLE IF EXISTS users;
 
+CREATE TABLE users (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20) NULL DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS rooms;
+
+CREATE TABLE rooms (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(40) NULL DEFAULT NULL
+);
+
+ALTER TABLE messages ADD FOREIGN KEY (user_key) REFERENCES users (id);
+ALTER TABLE messages ADD FOREIGN KEY (room_key) REFERENCES rooms (id);
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
+
 
